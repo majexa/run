1d289353275c97dd9ee7d2075f3a0b1334c998f8
masted <masted311@gmail.com>
2014-08-19 10:08:47 +0400

    Commit was made from server localhost by ci/push

--- tst.php
+++ tst.php
@@ -4,12 +4,14 @@ $params = implode(' ', array_slice($_SERVER['argv'], 1));
 $dir = __DIR__;
 if (isset($_SERVER['argv'][1]) and ($_SERVER['argv'][1] == 'proj' or $_SERVER['argv'][1] == 'plib')) {
   if (empty($_SERVER['argv'][3])) throw new Exception('projectName param #3 not defined');
-  if ($_SERVER['argv'][1] == 'plib') $includes = ' '.$_SERVER['argv'][4];
+  $offset = $_SERVER['argv'][1] == 'plib' ? -1 : 0;
+  if ($_SERVER['argv'][1] == 'plib') $includes = ' '.$_SERVER['argv'][4 + $offset];
   else $includes = '';
-  print `php $dir/run.php site {$_SERVER['argv'][3]} "new CliTestRunner('$params')"$includes`;
+  $cmd = "php $dir/run.php site {$_SERVER['argv'][3 + $offset]} \"new CliTestRunner('$params')\"$includes";
+  print `$cmd`;
 }
 else {
-  if (isset($_SERVER['argv'][1]) and $_SERVER['argv'][1] == 'lib') $includes = ' '.$_SERVER['argv'][3];
+  if (isset($_SERVER['argv'][1]) and $_SERVER['argv'][1] == 'lib') $includes = ' '.$_SERVER['argv'][2];
   else $includes = '';
   print `php $dir/run.php "new CliTestRunner('$params')"$includes`;
 }
