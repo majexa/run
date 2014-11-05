c58f8dd16b8b523e4e561bccb8b1fb48a76adc6a
masted <masted311@gmail.com>
2014-11-05 11:21:56 +0400

    Auto-commit from localhost

--- tst.php
+++ tst.php
@@ -4,10 +4,12 @@ $params = implode(' ', array_slice($_SERVER['argv'], 1));
 $dir = __DIR__;
 if (isset($_SERVER['argv'][1]) and ($_SERVER['argv'][1] == 'proj' or $_SERVER['argv'][1] == 'plib')) {
   if (empty($_SERVER['argv'][3])) throw new Exception('projectName param #3 not defined');
+  if ($_SERVER['argv'][1] == 'proj' and $_SERVER['argv'][2] == 'g') {
+    print `pm localServer createTestProject common`;
+  }
   $offset = $_SERVER['argv'][1] == 'plib' ? -1 : 0;
   if ($_SERVER['argv'][1] == 'plib') $includes = ' '.$_SERVER['argv'][4 + $offset];
   else $includes = '';
-  $projectName = $_SERVER['argv'][2];
   $cmd = "php $dir/run.php site {$_SERVER['argv'][3 + $offset]} \"new CliTestRunner('$params')\"$includes";
   print `$cmd`;
 }
