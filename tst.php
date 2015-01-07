e9eb0041124d4082131434889c9a70a5405caa8c
masted <masted311@gmail.com>
2015-01-07 19:57:57 +0300

    Auto-commit on 07.01.2015 20:57:57

--- tst.php
+++ tst.php
@@ -5,7 +5,8 @@ $dir = __DIR__;
 if (isset($_SERVER['argv'][1]) and ($_SERVER['argv'][1] == 'proj' or $_SERVER['argv'][1] == 'plib')) {
   if (empty($_SERVER['argv'][3])) throw new Exception('projectName param #3 not defined');
   if ($_SERVER['argv'][1] == 'proj' and $_SERVER['argv'][2] == 'g') {
-    print `pm localServer createTestProject common`;
+    $pm = 'php '.dirname($dir).'/pm/pm.php';
+    print `$pm localServer createTestProject common`;
   }
   $offset = $_SERVER['argv'][1] == 'plib' ? -1 : 0;
   if ($_SERVER['argv'][1] == 'plib') $includes = ' '.$_SERVER['argv'][4 + $offset];
